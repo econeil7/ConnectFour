c79d12628ad705de2efaea79c5678e2564347c94
econeil7 <ec.oneil93@gmail.com>
2021-04-11 07:54:23 -0400

    added placeToken, populateBoard, and createBoard methods and removed unnecessary variables

--- src/game/Grid.java
+++ src/game/Grid.java
@@ -1,20 +1,55 @@
 package game;
 
 public class Grid {
-	private int boardWidth = 7;
-	private int boardHeight = 6;
-	private int[][] grid;
+	// 2D array is upside down x,y grid
+	// think of X value increasing downwards, Y value increasing to the right
+	// Example placing of 'R' in [4][2] position
+	//	o o o o o o o 
+	//	o o o o o o o 
+	//	o o o o o o o 
+	//	o o o o o o o 
+	//	o o R o o o o 
+	//	o o o o o o o 
+	private char[][] board = new char[6][7];
+	public void populateBoard() {
+		for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                board[row][col] = 'o';
+            }
+        }
+	}
   
-  public String displayBoard() {
-    return "";
-  }
-  
-  public boolean checkForWin() {
-    return true;
-  }
-  
-  public void resetGrid() {
-    
-  }
+	public void displayBoard() {
+		for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+		System.out.println("-------------------------------------------------");
+	}
+	
+	public void placeToken(int chosenCol) {
+		boolean emptyCol = true;
+		for (int row = 0; row < board.length; row++) {
+			if (board[row][chosenCol] != 'o') {
+				board[row-1][chosenCol] = 'R';
+				emptyCol = false;
+				break;
+			}
+		}
+		if (emptyCol) {
+			board[5][chosenCol] = 'R';
+		}
+		displayBoard();
+	}
+	
+	public boolean checkForWin() {
+	  return true;
+	}
+	  
+	public void resetGrid() {
+	  
+	}
   
 }
