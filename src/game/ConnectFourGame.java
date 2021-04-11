368877611d375542caf3d0cd54283ed74227d0c2
econeil7 <ec.oneil93@gmail.com>
2021-04-11 07:53:42 -0400

    cleaned up main method and added light testing methods

--- src/game/ConnectFourGame.java
+++ src/game/ConnectFourGame.java
@@ -8,34 +8,14 @@ public class ConnectFourGame {
 		Player p1 = new Player(1, 'R');
 		Player p2 = new Player(2, 'Y');
 		Scanner ui = new Scanner(System.in);
-    
-	    while (true) {
-	      int column;
-	    System.out.println("Which column do you want to place your piece?");
-	      column = ui.nextInt();
-	      p1.placeToken(column);
-	    }
-	    
-		 // .......
-		 // .......
-		 // .......
-		 // .......
-		 // .R.....
-		 // RBBBB..
-	
-		 // grid[4][0] = 'B';
-	
-		 //for (int i=0; i<boardHeight; i++) {
-		 //  if (grid[i][col].equals('R') || grid[i][0].equals('B')) {
-		     
-		 //  }
-		   
-		   
-		   
-		 // for (int j=0; j<boardWidth; j++) {
-		   
-		 // }
-	    
+		Grid g = new Grid();
+		g.populateBoard();
+		g.placeToken(5);
+		g.placeToken(5);
+		g.placeToken(5);
+		g.placeToken(5);
+		g.placeToken(5);
+		g.placeToken(5);
 	}
 
 }
