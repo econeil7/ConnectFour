7263412958cbae4cbee9c1481af11eb839d41411
econeil7 <ec.oneil93@gmail.com>
2021-04-11 21:35:13 -0400

    added checkForFullColumn() method, clearBoard() method, and updated placeToken() and displayBoard()

--- src/game/Grid.java
+++ src/game/Grid.java
@@ -20,36 +20,71 @@ public class Grid {
 	}
   
 	public void displayBoard() {
+		System.out.println("0 1 2 3 4 5 6");
 		for (int row = 0; row < board.length; row++) {
             for (int col = 0; col < board[row].length; col++) {
                 System.out.print(board[row][col] + " ");
             }
             System.out.println();
         }
-		System.out.println("-------------------------------------------------");
+		System.out.println("-------------------------------------");
 	}
 	
-	public void placeToken(int chosenCol) {
+	public void placeToken(int chosenCol, char color) {
 		boolean emptyCol = true;
-		for (int row = 0; row < board.length; row++) {
-			if (board[row][chosenCol] != 'o') {
-				board[row-1][chosenCol] = 'R';
-				emptyCol = false;
-				break;
+		boolean result = checkForFullColumn(chosenCol);
+		if(result) 
+		{ // column not full insert
+			for (int row = 0; row < board.length; row++) {
+				if (board[row][chosenCol] != 'o') {
+					board[row-1][chosenCol] = color;
+					emptyCol = false;
+					break;
+				}
 			}
+			if (emptyCol) {
+				board[5][chosenCol] = color;
+			}
+			displayBoard();
 		}
-		if (emptyCol) {
-			board[5][chosenCol] = 'R';
-		}
-		displayBoard();
+		else //throw error
+        {
+            try {
+                throw new Exception("The column is full");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 	}
+	
+	public boolean checkForFullColumn(int col)
+    {
+        int row = 0;
+        System.out.println(col+"");
+        if (col < 0 || col > 7){
+            return false;
+        }
+
+        //full column?
+        for(int r = 0; r < 6; r++) {
+            if(board[r][col] == 'o') {
+                return true;
+            }
+        }
+        return false;
+    }
 	
 	public boolean checkForWin() {
 	  return true;
 	}
 	  
-	public void resetGrid() {
-	  
-	}
+	public void clearBoard()
+    {
+        for (int row = 0; row < board.length; row++){
+            for (int col = 0; col < board[0].length; col++){
+            	board[row][col] = 'o';
+            }
+        }
+    }
   
 }
