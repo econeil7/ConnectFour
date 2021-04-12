456c5c25ec33be6c5996c8541594d09f0339390a
econeil7 <ec.oneil93@gmail.com>
2021-04-11 21:34:19 -0400

    added basic back and forth turn taking for initial game setup

--- src/game/ConnectFourGame.java
+++ src/game/ConnectFourGame.java
@@ -7,15 +7,23 @@ public class ConnectFourGame {
 	public static void main(String[] args) {
 		Player p1 = new Player(1, 'R');
 		Player p2 = new Player(2, 'Y');
-		Scanner ui = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		Grid g = new Grid();
-		g.populateBoard();
-		g.placeToken(5);
-		g.placeToken(5);
-		g.placeToken(5);
-		g.placeToken(5);
-		g.placeToken(5);
-		g.placeToken(5);
+		boolean running = true;
+        g.populateBoard();
+        g.displayBoard();
+		while (running)
+        {
+            int col;
+            System.out.print("Player 1, please select a column: ");
+            col = scanner.nextInt();
+            g.placeToken(col, p1.getTokenColor());
+            System.out.println("Player 2, please select a column: ");
+            col = scanner.nextInt();
+            g.placeToken(col, p2.getTokenColor());
+        }
+        g.clearBoard();
+        g.populateBoard();
 	}
 
 }
